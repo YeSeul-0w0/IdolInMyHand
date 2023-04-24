d163c3175fb6158003545d47d5a1b985fa5caf99
YeSeul-0w0 <choi333ys@gmail.com>
2023-04-25 08:50:57 +0900

    create login function

--- app/src/main/java/com/example/idolinmyhand/Sign/LoginActivity.kt
+++ app/src/main/java/com/example/idolinmyhand/Sign/LoginActivity.kt
@@ -1,10 +1,19 @@
 package com.example.idolinmyhand.Sign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.idolinmyhand.MainActivity
 import com.example.idolinmyhand.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
@@ -12,5 +21,28 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
+        binding.login.setOnClickListener {
+            val email = binding.email.text.toString()
+            val passwd = binding.password.text.toString()
+            login(email, passwd)
+        }
     }
+     fun login(email:String, password:String) {
+         auth?.signInWithEmailAndPassword(email,password) // 로그인
+             ?.addOnCompleteListener {
+                     result->
+                 if(result.isSuccessful){
+                     var intent = Intent(this, MainActivity::class.java)
+                     startActivity(intent)
+                 }
+             }
+     }
+//    fun moveMainPage(user: FirebaseUser?){
+//        if( user!= null){
+//            startActivity(Intent(this,MainActivity::class.java))
+//            finish()
+//        }
+//    }
 }
+
